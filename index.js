38b6fbaef276f01e4ea8c6616f6275c51c2b7f34
YI KEE <marioxd1997@gmail.com>
2019-02-03 00:38:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,5 +33,9 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return (kittens.slice(1, 2));
+  return (kittens.slice(0, 2));
+}
+
+function removeFirstKitten() {
+  return (kittens.slice(2,3));
 }
