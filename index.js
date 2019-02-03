b2b9315357fafb8b5679299fc9928404aa5fd67b
YI KEE <marioxd1997@gmail.com>
2019-02-03 00:23:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  var ralph = "Ralph";
   kittens.push(ralph);
   return kittens;
 }
